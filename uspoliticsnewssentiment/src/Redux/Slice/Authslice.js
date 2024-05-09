321013f1d462b070a0ae049701656d6248c80a12
shehnaz <167173129+shehnazwebpaint@users.noreply.github.com>
2024-05-09 19:12:20 +0530

    implemmented pagination to show next articles and dropdown show latest articles

--- uspoliticsnewssentiment/src/Redux/Slice/Authslice.js
+++ uspoliticsnewssentiment/src/Redux/Slice/Authslice.js
@@ -17,30 +17,18 @@ const authSlice = createSlice({
     name: 'news',
     initialState,
     reducers: {
-        // ArticleSlice: (state, action) => {
-        //     state.ArticlesData = action.payload;
-        //     state.totalPositiveCount = action.payload.next.totalResults; 
-        // },
-
-        // NegativeArticleSlice: (state, action) => {
-        //     state.NegativearticleData = action.payload;
-        //     state.totalNegativeCount =action.payload.next.totalResults
-        // },
+       
         ArticleSlice: (state, action) => {
             state.ArticlesData = action.payload;
             state.totalPositiveCount = action.payload.totalResults;
-            state.nextPositive = action.payload.next; // Assuming 'next' is correctly part of the payload
+            state.nextPositive = action.payload.next; 
         },
 
         NegativeArticleSlice: (state, action) => {
             state.NegativearticleData = action.payload;
             state.totalNegativeCount = action.payload.totalResults;
-            state.nextNegative = action.payload.next; // Similarly for negative
+            state.nextNegative = action.payload.next; 
         },
-        // NextpaginationSlice: (state, action) => {
-        //     state.Nextpaginationdata = action.payload;
-        //     state.totalNegativeCount =action.payload.totalResults
-        // },
         setLoading: (state, action) => {
             state.loading = action.payload;
         },
