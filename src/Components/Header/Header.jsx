6987104d0c364104d03db44874171f580cc168e3
360webexpert <360webexpert@gmail.com>
2024-05-16 12:26:20 +0530

    restructure

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">
+                        <img src='/Images/logo.svg' alt='' className="img-fluid" />
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <img src='/Images/nav-toggle.svg' alt='' className="img-fluid" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/howitworks">How it Works</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/accesstoken">Access Token</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/faq">FAQ's</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="#">Contact</Link>
+                            </li>
+                        </ul>
+
+                        {/* <span className="btn btn-warning" role="button" tabIndex="0">
+                            Get API | Access Token
+                        </span> */}
+                        <a href="https://webz.io/products/news-api#lite" target="_blank" rel="noopener noreferrer">
+                            <button className="btn btnOne" >  Get API  Access Token</button>
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
---
+++ src/Components/Homepage/Chart/Chart.js
@@ -0,0 +1,51 @@
+import React,{useEffect} from 'react';
+import { people } from '../Newscard/PeopleData';
+import PieChart from '../../../Piechart/Piechart';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { ArticledisplayAction } from '../../../Redux/Action/Authaction';
+import { PietotalresultAction} from '../../../Redux/Action/Authaction';  
+
+
+export default function Chart() {
+    const dispatch = useDispatch();
+    const pietotalPositiveCount = useSelector((state) => state.news.pietotalPositiveCount);
+    const pietotalNegativeCount = useSelector((state) => state.news.pietotalNegativeCount);
+    // console.log(pietotalPositiveCount,pietotalNegativeCount,"hhhhhhhhhhh")
+
+    useEffect(() => {
+        dispatch(PietotalresultAction());
+    }, [dispatch]);
+
+    return (
+        <section className='chart-section bg_gray'>
+            <div className="container">
+                <div className='row'>
+                    <div className="col-lg-6 mb-5 mb-lg-0">
+                        {/* <PieChart articles={aggregatedArticles} context="home" /> */}
+                         <PieChart totalPositiveCount={pietotalPositiveCount} totalNegativeCount={pietotalNegativeCount} context="home" />
+                    </div>
+
+                    <div className="col-lg-6 section-title pe-xxl-1">
+                        <h2>Tracking the Pulse: </h2>
+                        <h3>Viewer Sentiment Analysis of the US Election</h3>
+                        <div class="title-description me-xxl-2 text-center text-lg-start">
+                            <p className='mb-3 pb-1'>Webz.io sources and collects data from across the web and transforms it into machine-ready feeds that plug right into any platform. It deploys a wide array of crawlers that run in near real-time, drawing from millions of sources — covering everything from the biggest news sites, to obscure blogs and forums, all the way to the furthest reaches of the dark web.</p>
+
+                            <p>All stored in repositories, so machines consume live and historical data on demand. Webz.io gives machines data exactly the way they need it, so companies easily turn web data into customer value.</p>
+                        </div>
+
+                        <div className="buttons-holder text-center text-lg-start">
+                           <a href="https://webz.io/products/news-api#lite" target="_blank" rel="noopener noreferrer">
+                            <button className="btn btnTwo" role="button" tabIndex="0">
+                                Get API Access Token
+                            </button>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
