b920e20e41c4bef5c9ca5cdeb1b5365fed21cb8a
shehnaz <167173129+shehnazwebpaint@users.noreply.github.com>
2024-05-16 11:30:05 +0530

    changes::new

--- uspoliticsnewssentiment/src/Redux/Slice/Authslice.js
+++ uspoliticsnewssentiment/src/Redux/Slice/Authslice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    ArticlesData: [],
+    ArticlesData: [],  
     NegativearticleData:[],
     // Nextpaginationdata:[] ,
     totalPositiveCount: 0, 
